0bc76cf974fe2bf969ff7ecb43a9e05e312d554f
flyaway12345 <evanhaggard123@gmail.com>
2025-01-07 13:25:19 -0800

    implemetnted hiding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,25 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'griptape-adventure';
-
-
+  landingPage:boolean = false;
+  MP1:boolean = false;
+  MP2:boolean = false;
+  MP3:boolean = false;
+  MP4:boolean = false;
+  MP4D1:boolean = false;
+  MP4D2:boolean = false;
+  MP4D3:boolean = false;
+  MP4D4:boolean = false;
+  MP4D4P1:boolean = false;
+  MP4D4P2:boolean = false;
+  MP4D4P3:boolean = false;
+  MP4D4P4:boolean = false;
+  MP4L1:boolean = false;
+  MP4L2:boolean = false;
+  MP4L3:boolean = false;
+  lastPage:boolean = false;
+onClickM1ToM2(){
+this.MP1 = false;
+this.MP2 = true;
+}
 }
