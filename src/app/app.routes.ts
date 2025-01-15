4e7116f600b402ba8a36eea0a56aa5c0e8a0ff33
flyaway12345 <evanhaggard123@gmail.com>
2025-01-15 11:32:07 -0800

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NycComponent } from './nyc/nyc.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    // {path:'NYC', component:NycComponent} 
+    {path:'NYC', component:NycComponent}
 ];
