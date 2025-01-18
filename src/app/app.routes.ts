31dac98ad5151a204b12cf96d29cb892f80eabe8
flyaway12345 <evanhaggard123@gmail.com>
2025-01-17 18:16:41 -0800

    added new componet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { NycComponent } from './nyc/nyc.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TexasComponent } from './texas/texas.component';
 
 export const routes: Routes = [
     {path:'NYC', component:NycComponent},
     {path:'Nyc', component:NycComponent},
     {path:'nyc', component:NycComponent},
+    {path:'Texas', component:TexasComponent},
+    {path:'TEXAS', component:TexasComponent},
+    {path:'texas', component:TexasComponent},
     {path:'**', component:LandingPageComponent}
 ];
